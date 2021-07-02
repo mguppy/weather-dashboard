63fe7a943f3cccf1f32d7008430e2f20c5586376
mguppy <mguppy34@gmail.com>
2021-07-02 16:20:41 -0500

    layout updated

---
+++ logic.js
@@ -0,0 +1,14 @@
+// The user can type a city into a form and click the "Search" button and be presented with the current and future conditions for the city
+    //Need button click event listener
+// The city is then added to local Storage as part of a cities array
+    //Need local storage for cities array
+// The current weather conditions for the city should include the city, date, icon representing the weather conditions, temperature, humidity, wind speed and UV index
+    // APIs for current weather - Current Weather Data, Solar Radiation API (might need one call API instead now), icon one(?)
+// For the UV index, a color is also presented representing favorable, moderate, or severe
+    //Need if statement
+// Future weather conditions should also appear for that city
+    // APIs for future - 5 Day / 3 Hour Forecast
+// For the 5 day forecast, the following should be included: date, icon representing weather conditions, temperature, wind speed and humidity
+    // Bootstrap card layout can be used here
+// User should be able to click on the city in search history and the data should pull back again for that city
+    // Get items from local storage
